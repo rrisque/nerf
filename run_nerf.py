fead0f8e85040f4f54623966f11367536b28de21
Rob <rob.risque@gmail.com>
2020-04-03 10:39:27 -0400

    cleanup

--- run_nerf.py
+++ run_nerf.py
@@ -58,29 +58,6 @@ def render_rays(ray_batch,
                 raw_noise_std=0.,
 #                 netchunk=1024*64,
                 verbose=False):
-    
-#     def batchify(fn, chunk=netchunk):
-#         if chunk is None:
-#             return fn
-#         def ret(inputs):
-#             return tf.concat([fn(inputs[i:i+chunk]) for i in range(0, inputs.shape[0], chunk)], 0)
-#         return ret
-    
-#     def run_network(inputs, N_vdirs=None, fn=network_fn):
-#         inputs_flat = tf.reshape(inputs, [-1, inputs.shape[-1]])
-            
-#         embedded = embed_fn(inputs_flat)
-#         if embeddirs_fn is not None:
-#             viewdirs = ray_batch[:,-3:]
-#             input_dirs = tf.broadcast_to(viewdirs[:,None], inputs.shape)
-#             input_dirs_flat = tf.reshape(input_dirs, [-1, input_dirs.shape[-1]])
-#             embedded_dirs = embeddirs_fn(input_dirs_flat)
-#             embedded = tf.concat([embedded, embedded_dirs], -1)
-            
-#         if verbose: print('embedded range', embedded.numpy().min(), embedded.numpy().max(), embedded.numpy().mean())
-#         outputs_flat = batchify(fn)(embedded)
-#         outputs = tf.reshape(outputs_flat, list(inputs.shape[:-1]) + [outputs_flat.shape[-1]])
-#         return outputs
         
         
     def raw2outputs(raw, z_vals, rays_d):
